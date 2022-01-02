25f24f4468545e7449f829141136754e78d4d976
dhalladay <dave.halladay@outlook.com>
2022-01-02 15:50:12 -0700

    initial setup

---
+++ app.js
@@ -0,0 +1,5 @@
+//create team member constructor that others can inherit from
+//create manager object constructor that inherits from team member 
+//create engineer object
+//create intern object
+//create 
