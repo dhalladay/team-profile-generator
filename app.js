d9c25755f319f7cbf350deab795ccd4429c33a6a
dhalladay <dave.halladay@outlook.com>
2022-01-07 23:08:00 -0700

    update directory structure and begin prompts

--- app.js
+++
@@ -1,5 +0,0 @@
-//create team member constructor that others can inherit from
-//create manager object constructor that inherits from team member 
-//create engineer object
-//create intern object
-//create 
---
+++ index.js
@@ -0,0 +1,65 @@
+//require node packages
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+//require constructors
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+//declare array for team creation
+function CreateTeam() {
+  this.team = [];
+}
+
+CreateTeam.prototype.startPrompt = function() {
+
+  return inquirer
+    .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: "What is the team manager's name?",
+      validate: managerNameInput => {
+        if (managerNameInput) {
+          return true;
+        } else {
+          console.log("You must enter the manager's name.");
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'manager-id',
+      message: "What is the team manager's employee ID?",
+      validate: managerIdInput => {
+        if (managerIdInput) {
+          return true;
+        } else {
+          console.log("You must enter the manager's ID.");
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'manager-email',
+      message: "What is the team manager's email?",
+      validate: managerEmailInput => {
+        if (managerEmailInput) {
+          return true;
+        } else {
+          console.log("You must enter the manager's ID.");
+          return false;
+        }
+      }
+    },
+  ])
+};
+
+new CreateTeam()
+  .startPrompt()
+  .then(data => {
+    console.log(data)
+  });
